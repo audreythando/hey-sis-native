cdacfcd1d7424a24080066d51dfa44d0ece96369
audreythando <audreythando@gmail.com>
2022-10-28 19:07:57 +0200

    refactor:created new splash screen , icons

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import{NavigationContainer} from '@react-navigation/native'
+import TabNavigation from './src/components/TabNavigation';
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      
+      <TabNavigation/>
     </NavigationContainer>
   );
 }
